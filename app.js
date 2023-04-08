51730a26df3ea933e97e31eacc585f4604ddb236
PRASANTH REDDY NANDHARAPU <109941441+PRASANTH-REDDY-NANDHARAPU@users.noreply.github.com>
2023-04-08 21:37:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("welcome to btc")
+function login(){
+    let username =document.getElementById('username').value
+    let password =document.getElementById('password').value
+    // console.log(username)
+    // console.log(password)
+    if(username== 'prasanth'&& password=='katrina143prasanth'){
+        open('instagram.html')
+    }
+    else{
+        alert('areyy hoo stupid.....you rae not a valid person👊🏻👃🥊')
+    }
+
+}
